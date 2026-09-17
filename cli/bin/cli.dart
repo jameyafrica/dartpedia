5c435e8b50a7b15f455d4ff6ab55a3793c2abce9
Jamey <jaafrcpt025@student.wethinkcode.co.za>
2026-09-17 15:47:52 +0200

    feat: completed lesson 6

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,18 +1,17 @@
-// import 'package:cli/cli.dart' as cli;
-import 'dart:io';
-import 'package:http/http.dart' as http; 
 import 'package:command_runner/command_runner.dart';
 
+void main() {
+  final verboseOption = Option(
+    'verbose',
+    type: OptionType.flag,
+    abbr: 'v',
+    help: 'Display extra logging information.',
+  );
 
-import 'dart:io';
-import 'package:http/http.dart' as http;
-import 'package:command_runner/command_runner.dart';
-
-void main(List<String> arguments) async { // main is now async and awaits the runner
-  var runner = CommandRunner(); // Create an instance of your new CommandRunner
-  await runner.run(arguments); // Call its run method, awaiting its Future<void>, passing in the command line arguments
-
+  print('Defined option: ${verboseOption.name}');
+  print('Usage: ${verboseOption.usage}');
 }
+
 
 //  const version = '0.0.1';
 // void main(List<String> arguments) {
