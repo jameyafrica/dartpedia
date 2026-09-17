d06a5491ffa16f307518267692463ba99a195d05
Jamey <jaafrcpt025@student.wethinkcode.co.za>
2026-09-17 15:55:13 +0200

    feat: completed lesson 7

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,15 +1,10 @@
 import 'package:command_runner/command_runner.dart';
 
-void main() {
-  final verboseOption = Option(
-    'verbose',
-    type: OptionType.flag,
-    abbr: 'v',
-    help: 'Display extra logging information.',
-  );
+const version = '0.0.1';
 
-  print('Defined option: ${verboseOption.name}');
-  print('Usage: ${verboseOption.usage}');
+void main(List<String> arguments) {
+  var commandRunner = CommandRunner()..addCommand(HelpCommand());
+  commandRunner.run(arguments);
 }
 
 
