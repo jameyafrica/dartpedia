20a93c70c117e8358e114cfcc4feb86cfa42fc1d
Jamey <jaafrcpt025@student.wethinkcode.co.za>
2026-09-21 14:59:19 +0200

    feat: completed lesson 10

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -3,8 +3,10 @@ import 'package:command_runner/command_runner.dart';
 const version = '0.0.1';
 
 void main(List<String> arguments) {
-  // [Step 6 update] Add onError method
   var commandRunner = CommandRunner(
+    onOutput: (String output) async {
+      await write(output);
+    },
     onError: (Object error) {
       if (error is Error) {
         throw error;
@@ -16,6 +18,7 @@ void main(List<String> arguments) {
   )..addCommand(HelpCommand());
   commandRunner.run(arguments);
 }
+
 
 
 //  const version = '0.0.1';
